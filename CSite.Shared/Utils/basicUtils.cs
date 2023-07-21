e92e604f9f343b32c35c8547ee95c744c395bfad
Amin Norollah <amin.norollah@gmail.com>
2023-07-21 19:31:31 +0330

    add: summary to function

--- CSite.Shared/Utils/basicUtils.cs
+++ CSite.Shared/Utils/basicUtils.cs
@@ -11,23 +11,29 @@ namespace GitActivity.utils
         //adding new functions that can used in our other projects
         public basicUtils() { }
 
+        /// <summary>
+        /// Calculates the average of a list of double values.
+        /// </summary>
+        /// <param name="numbers">The list of numbers to calculate the average of.</param>
+        /// <returns>The average of the values in the input list.</returns>
+        /// <exception cref="ArgumentException">Thrown when the input list is null or empty.</exception>
         public double CalculateAverage(List<double> numbers)
-{
-    if (numbers == null || numbers.Count == 0)
-    {
-        throw new ArgumentException("Cannot calculate average of empty list");
-    }
+        {
+            if (numbers == null || numbers.Count == 0)
+            {
+                throw new ArgumentException("Cannot calculate average of empty list");
+            }
 
-    double sum = 0;
+            double sum = 0;
 
-    foreach (double num in numbers)
-    {
-        sum += num;
-    }
+            foreach (double num in numbers)
+            {
+                sum += num;
+            }
 
-    return sum / numbers.Count;
-}
+            return sum / numbers.Count;
+        }
 
- /**/
+        /**/
     }
 }
